0e1f1793032d822213f593f5795f7d7e6166b82c
Laurence Bahiirwa <laurence@mediauganda.com>
2020-07-18 17:23:46 +0300

    Update building-post-endpoint.php

--- building-post-endpoint.php
+++ building-post-endpoint.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: TechiePress Callaback URL Receive
  * Plugin URI: https://omukiguy.com
@@ -9,6 +10,12 @@
  * License: GPL2 or later
  * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
  * text-domain: prefix-plugin-name
+ * 
+ * Credits:
+ * This Repository shows the code for the Youtube Video series https://www.youtube.com/playlist?list=PLNqG1qGUllk0npc2ZGPU358Q6S1itFCYR on the use of the WordPress REST API to handle Callback URLs from another server.
+ * Have you have ever wanted to use your WordPress or its plugins to receive data from a callback URL or Webhooks?
+ * In this video series, we explore how to use the WordPress REST API to receive the sent data from an External API, webhook URL or a callback URL. Then use the code to make it do a bunch of things for you.
+ * In the following series, you will learn how to make an API or an API.
 */
  
 add_action( 'rest_api_init', 'techiepress_add_callback_url_endpoint' );
